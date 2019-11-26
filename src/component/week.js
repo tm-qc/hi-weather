6560ee4aa74c341719781916fa40e8f8cb439979
tm-qc <web.tm.mail@gmail.com>
2019-11-26 23:39:32 +0900

    °追記

--- src/component/week.js
+++ src/component/week.js
@@ -13,8 +13,8 @@ export default{
         <div v-if="index!==0" class="weekCard__weekDate">
           <div class="weekCard__timing-week">{{index}}day</div>
           <div class="weekCard__date">{{timeStampCompile(d.data[index].time,"day")}}</div>
-          <div class="weekCard__tempMax">{{mathRound(d.data[index].temperatureHigh)}}</div>
-          <div class="weekCard__tempMin">{{mathRound(d.data[index].temperatureLow)}}</div>
+          <div class="weekCard__tempMax">{{mathRound(d.data[index].temperatureHigh)}}°</div>
+          <div class="weekCard__tempMin">{{mathRound(d.data[index].temperatureLow)}}°</div>
           <div class="weekCard__iconAreaWeek">
             <prefix_icons :weatherInfo=d.data[index].icon></prefix_icons>
           </div>
